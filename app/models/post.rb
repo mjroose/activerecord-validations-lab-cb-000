76673b31cb03393c1baf231efe5ebf197e4503e1
Michael Roose <mjroose@gmail.com>
2018-11-17 19:50:42 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,6 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 
   def click_bait?
-    binding.pry
     t = title
     !!t && (!!t.index("Won't Believe") || !!t.index("Secret") || !!t.index(/Top\s\d/) || !!t.index("Guess"))
   end
