b2d3e60a2b72eebd3482020a28815209a2d93ba7
Michael Roose <mjroose@gmail.com>
2018-11-17 19:46:04 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates_with click_bait?
+  validates_with self.click_bait?
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
