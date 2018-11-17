8a2c197d28495cf8c33a87537bf60f8e27ef950d
Michael Roose <mjroose@gmail.com>
2018-11-17 20:07:11 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validate :click_bait?
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+  def click_bait?
+    t = title
+    if !t || !(!!t.index("Won't Believe") || !!t.index("Secret") || !!t.index(/Top\s\d/) || !!t.index("Guess"))
+      errors.add(:title, "must be clickbait-y")
+    end
+  end
 end
