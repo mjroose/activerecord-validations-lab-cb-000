5318f4cb6372f11fbe5e5401a04bea9378c7d67d
Michael Roose <mjroose@gmail.com>
2018-11-17 19:45:15 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,11 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
+  validates_with click_bait?
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 
-  def validate(record)
+  def click_bait?(record)
     t = record.title
     !!t.index("Won't Believe") || !!t.index("Secret") || !!t.index(/Top\s\d/) || !!t.index("Guess")
   end
